ef28f90c136da5f2aa73ad12fc78e4d936f12422
luisespriella9 <lfd2tb@virginia.edu>
2019-09-03 13:21:07 -0400

    implemented direct graph

---
+++ graph.py
@@ -0,0 +1,33 @@
+﻿class GraphNode:
+    name = ""
+    children = []
+    def __init__(self, name):
+        self.name = name
+
+    def addEdge(self, child):
+        self.children.append(child)
+
+class Graph:
+    #implementation of directed graph
+    nodes = []
+
+    def addNode(self, name):
+        graphNode = graphNode(name)
+        self.nodes.append(graphNode)
+
+    def addEdge(self, pointingNodeName, pointerNodeName):
+        #find two nodes if they exists, if not then do nothing
+        pointerNode = None
+        pointingNode = None
+        for node in nodes:
+            if (node.name == pointingNodeName):
+                pointingNode = node
+            if (node.name == pointerNodeName):
+                pointerNode = node
+        if (pointerNode == None or pointingNode == None):
+            return
+        #add edge from pointing node to the pointer node
+        pointingNode.addEdge(pointerNode)
+
+if __name__ == "__main__":
+    print("running main")
